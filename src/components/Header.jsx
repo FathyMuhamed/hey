9d45ea87ba12949fe537cf6c9a9e196f0575bbdc
FathyMuhamed <fathimuhamed10522@gmail.com>
2024-11-24 20:46:05 +0200

    Add animated header for "DAYS I CODE" in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -102,6 +102,14 @@ export default function Header() {
         >
           RECENT WORK
         </motion.h4> */}
+        <motion.h4
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2 }}
+          className="mt-3 text-sm text-neutral-500 max-w-2xl  leading-relaxed"
+        >
+          DAYS I CODE
+        </motion.h4>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
